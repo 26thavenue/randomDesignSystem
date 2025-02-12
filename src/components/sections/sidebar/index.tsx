8a322586c97e0a9e98cb0371795a914c411cb945
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-02-12 16:19:05 +0100

    Working

--- src/components/sections/sidebar/index.tsx
+++ src/components/sections/sidebar/index.tsx
@@ -1,29 +1,74 @@
 import { Link } from "react-router-dom";
+import { Divider } from "../..";
 
 const Sidebar = () => {
   return (
-    <aside className="w-64 min-h-screen bg-gray-900 text-white p-6 fixed">
-      <h2 className="text-lg font-semibold mb-4">Docs Navigation</h2>
+    <aside className="w-64 min-h-screen bg-orange-600 text-white p-6 fixed">
+      <h2 className="text-xl font-semibold mb-4">Docs Navigation</h2>
       <nav>
         <ul className="space-y-3">
-          <li>
-            <Link to="/docs" className="block hover:text-gray-300">
-              Introduction
-            </Link>
-          </li>
-          <li>
-            <Link to="/docs/buttons" className="block hover:text-gray-300">
-              Buttons
-            </Link>
-          </li>
-          <li>
-            <Link to="/docs/alerts" className="block hover:text-gray-300">
-              Alerts
-            </Link>
-          </li>
+          <div className="flex flex-col gap-2">
+            <p className="text-lg font-semibold">UI</p>
+            <div className="pl-6 flex">
+                <Divider orientation="vertical"/>
+                <div className="flex flex-col items-start gap-1 ">
+                  <li className="hover:underline transition">
+                    <Link to="/docs/alerts"> Alerts</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/avatars"> Avatars</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/badges"> Badges</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/buttons"> Buttons</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/cards"> Cards</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/chips"> Chips</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/dividers"> Dividers</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/indicators"> Indicators</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/inputs"> Inputs</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/progress"> Progress</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/ratings">Ratings</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/skeletons"> Skeletons</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/spinners"> Spinners</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/tabs"> Tabs</Link>
+                  </li>
+                  <li>
+                    <Link to="/docs/tooltips"> Tooltips</Link>
+                  </li>
+                </div>
+                
+            </div>
+           
+          </div>
+          
+          <p>Section</p>
+          <p>Base Components</p>
+          <p>Animations</p>   
         </ul>
       </nav>
-    </aside>
+    </aside>  
   );
 };
 
--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,18 +1,21 @@
 import { MDXProvider } from "@mdx-js/react";
-import { Button } from "../components";
 import DocsLayout from "./DocsLayout";
 
 type DocsProps = {
   Component: React.ComponentType;
 };
+
 const components = {
-  Button, 
+  h1: (props:any) => <h1 className="text-4xl font-bold mt-6 mb-4" {...props} />,
+  h2: (props:any) => <h2 className="text-3xl font-semibold mt-5 mb-3" {...props} />,
+
 };
+
 
 const Docs = ({ Component }: DocsProps) => {
   return (
     <DocsLayout>
-      <MDXProvider components={components}>
+      <MDXProvider  components={components} >
         <div className="docs-container">
           <Component />
         </div>
